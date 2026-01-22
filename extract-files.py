9a823f34a17c8021deafda9cba0994ecd9890d54
AnierinB <anierin@evolution-x.org>
2026-01-22 10:24:48 +0000

    odin2mini: Initial vendor blobs
    Change-Id: Iebad0186b44fdb2db127ae2e35005f63a744013f
    Signed-off-by: AnierinB <anierin@evolution-x.org>

--- extract-files.py
+++ extract-files.py
@@ -4,6 +4,11 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+from extract_utils.fixups_blob import (
+    blob_fixup,
+    blob_fixups_user_type,
+)
+
 from extract_utils.main import (
     ExtractUtils,
     ExtractUtilsModule,
@@ -13,10 +18,17 @@ namespace_imports = [
     'vendor/ayn/qcs8550-common',
 ]
 
+blob_fixups: blob_fixups_user_type = {
+    'vendor/etc/display/qdcm_calib_data_xm91080_video_mode_dsi_panel_without_DS.json': blob_fixup()
+        .regex_replace('nt35532_video_mode_dsi_panel_without_DSC', 'xm91080_video_mode_dsi_panel_without_DSC'),
+}  # fmt: skip
+
 module = ExtractUtilsModule(
     'odin2mini',
     'ayn',
+    blob_fixups=blob_fixups,
     namespace_imports=namespace_imports,
+    add_firmware_proprietary_file=True,
 )
 
 if __name__ == '__main__':
